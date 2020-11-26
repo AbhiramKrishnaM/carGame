bceb782b526ff61a4a903a71b5eef27e3cd515ab
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2020-11-26 17:35:54 +0530

    Update test.js
    chnaged to new version

--- test.js/test.js
+++ test.js/test.js
@@ -35,7 +35,7 @@ function gameApp(){
 
         for(var i = 0; i < pumpLocation.length; i++){
 
-            if(traversedDistance == pumpLocation[i] || ((traversedDistance >= pumpLocation[i]) && (traversedDistance <= pumpLocation[i+1])) )
+            if(traversedDistance == pumpLocation[i] || ((traversedDistance > pumpLocation[i]) && (traversedDistance < pumpLocation[i+1])) )
             {
                 console.log(i);  
                 diff = Math.abs(traversedDistance - pumpLocation[i]);
